c229f80531c26eed055500ada32ff34b5d2294d5
Султан <carameleclipsimus@gmail.com>
2023-04-21 16:22:10 +0600

    feat: second task of this is done

--- script.js
+++ script.js
@@ -8,16 +8,52 @@
 const rectangle = {width:..., height:..., getSquare:...};
 */
 
-function getSquare() {
-  console.log(this.width);
-  console.log(this.height);
-  console.log(this.width * this.height);
-  return this;
+// function getSquare() {
+//   console.log(this.width);
+//   console.log(this.height);
+//   console.log(this.width * this.height);
+//   return this;
+// }
+// const rectangle = {
+//   width: 10,
+//   height: 10,
+//   getSquare,
+// };
+// const getSquareVar = rectangle.getSquare();
+
+// second
+
+/*
+Создать объект, у которого будет цена товара и его скидка, а также
+
+два метода: для получения цены и для расчета цены с учетом скидки:
+
+const price = {
+
+price: 10,
+
+discount: '15%',
+
+... };
+
+price.getPrice(); // 10
+
+price.getPriceWithDiscount(); // 8.5
+*/
+function getPrice() {
+  console.log(this.price);
 }
-const rectangle = {
-  width: 10,
-  height: 10,
-  getSquare,
+function getPriceWithDiscount() {
+  const value = (this.price / 100) * parseInt(this.discount);
+  const getPriceDis = this.price - value;
+  console.log(getPriceDis);
+}
+const price = {
+  price: 10,
+  discount: "15%",
+  getPrice,
+  getPriceWithDiscount,
 };
-const getSquareVar = rectangle.getSquare();
-console.log(getSquareVar);
+
+price.getPrice();
+price.getPriceWithDiscount();
